7cb89ed9ce852a4e888cd0355dcbdef615b0176a
Duncan Skinner <skinnerduncan@gmail.com>
2015-01-22 13:07:03 -0800

    board clears full rows

--- lib/board.rb
+++ lib/board.rb
@@ -1,3 +1,32 @@
 class Board
+  attr_reader :grid
   
+  def initialize
+    @grid = Array.new(10){ Array.new(10, " ") }
+  end
+  
+  def clear_full_rows 
+    cleared = false
+    (0..9).each do |row|
+      if @grid[row].all?{ |square| square == "X" }
+        clear_full_row(row) 
+        cleared = true
+      end
+    end
+    
+    cleared
+  end
+  
+  def clear_full_row(full_row)
+    full_row.downto(1).each do |row|
+      @grid[row] = @grid[row - 1]
+    end
+    @grid[0] = Array.new(10, " ")
+  end
+  
+  def display
+    @grid.each do |row|
+      p row
+    end
+  end
 end
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -1,5 +1,57 @@
 require 'board'
 
 describe Board do
+  describe "#initialize" do
+    it "creates new board with ten by ten grid" do 
+      board = Board.new
+      expect(board.grid.length).to eq(10)
+      board.grid.each do |row|
+        expect(row.length).to eq(10)
+      end
+    end
+  end
   
+  describe "#clear_full_rows" do     
+    it "clears all full rows" do
+      board = Board.new
+      full_rows = [6, 8, 9]
+      full_rows.each do |row|
+        (0..9).each{ |col| board.grid[row][col] = "X" }
+      end 
+      
+      board.clear_full_rows
+      full_rows.each do |row|
+        board.grid[row].each do |square|
+         expect(square).to eq(" ") 
+        end
+      end
+    end
+    
+    it "cascades higher rows downward" do
+      board = Board.new
+      full_rows = [6, 8, 9]
+      full_rows.each do |row|
+        (0..9).each{ |col| board.grid[row][col] = "X" }
+      end 
+      
+      (0..7).each{ |col| board.grid[5][col] = "X"} 
+      (1..9).each{ |col| board.grid[7][col] = "X"} 
+      board.display
+      board.clear_full_rows
+      board.display
+      expect(board.grid[9]).to eq([" ", "X", "X", "X", "X", "X", "X", "X", "X", "X"])
+      expect(board.grid[8]).to eq(["X", "X", "X", "X", "X", "X", "X", "X", " ", " "])
+    end
+    
+    it "adds empty rows on top" do
+      board = Board.new
+      full_rows = [6, 8, 9]
+      full_rows.each do |row|
+        (0..9).each{ |col| board.grid[row][col] = "X" }
+      end 
+      
+      board.clear_full_rows
+      expect(board.grid.length).to eq(10)
+    end
+  end
 end
