aa5bacdb29030ef962329009dc0101bb7034294b
Duncan Skinner <skinnerduncan@gmail.com>
2015-01-22 15:54:42 -0800

    game knows where to place block

--- lib/game.rb
+++ lib/game.rb
@@ -1,3 +1,26 @@
+require_relative 'board'
+
 class Game
+  attr_reader :board
   
+  def initialize
+    @board = Board.new
+    @next_piece = Block.new
+  end
+  
+  def find_empty_column
+    (0..(10 - @next_piece.width)).each do |col|
+      col_empty = true
+      @next_piece.width.times do |wid|
+        if @board.grid.any?{ |row| row[col + wid] == "X" }
+          col_empty = false
+          break
+        end
+      end
+      
+      return col if col_empty
+    end
+    
+    false
+  end
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,5 +1,16 @@
 require 'game'
 
 describe Game do
-  
+  describe "#find_empty_column" do
+    subject(:game) { game = Game.new }
+
+    it "chooses column 0 when the grid is empty" do
+      expect(game.find_empty_column).to eq(0)
+    end
+    
+    it "chooses the first empty column when the grid is not empty" do
+      game.board.place_piece(Block.new, 0)
+      expect(game.find_empty_column).to eq(2)
+    end
+  end
 end
