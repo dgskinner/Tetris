18338a2ff0006cb1d5d172e5dd39edfdb5f7fa9a
Duncan Skinner <skinnerduncan@gmail.com>
2015-01-22 15:00:23 -0800

    pieces know their height and width and can rotate

--- lib/piece.rb
+++ lib/piece.rb
@@ -1,3 +1,21 @@
 class Piece
+  attr_accessor :view
   
+  def height 
+    @view.length
+  end
+  
+  def width
+    @view[0].length
+  end
+  
+  def display
+    @view.each do |row|
+      p row
+    end
+  end
+  
+  def rotate!
+    @view = @view.transpose.map{ |row| row.reverse }
+  end
 end
--- spec/piece_spec.rb
+++ spec/piece_spec.rb
@@ -1,5 +1,19 @@
 require 'piece'
 
 describe Piece do
+  it "has correct height and width" do
+    custom_piece = Piece.new
+    custom_piece.view = [["X", "X"], ["X", "X"], ["X", "X"]]
+    expect(custom_piece.height).to eq(3)
+    expect(custom_piece.width).to eq(2)
+  end
   
+  describe "#rotate!" do 
+    it "rotates a custom piece" do 
+      custom_piece = Piece.new
+      custom_piece.view = [["X", "X", "X"], ["X", "X", " "], ["X", " ", " "]]
+      custom_piece.rotate!
+      expect(custom_piece.view).to eq([["X", "X", "X"], [" ", "X", "X"], [" ", " ", "X"]])
+    end
+  end
 end
