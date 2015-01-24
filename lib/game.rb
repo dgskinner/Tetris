48d1ca53a1c6e53d4a455b2aa55201c8b7a02a50
Duncan Skinner <skinnerduncan@gmail.com>
2015-01-23 17:48:52 -0800

    if no empty column, game finds best column instead of choosing at random

--- lib/game.rb
+++ lib/game.rb
@@ -20,11 +20,22 @@ class Game
     @board.display
   end
   
-  def find_empty_column
+  def find_best_column
+    lowest_row = 9
+    empty_column = false
+    while true
+      empty_column = find_empty_column(lowest_row)
+      break if empty_column
+      lowest_row -= 1
+    end
+    empty_column
+  end
+  
+  def find_empty_column(lowest_row = 9)
     (0..(10 - @next_piece.width)).each do |col|
       col_empty = true
       @next_piece.width.times do |wid|
-        if @board.grid.any?{ |row| row[col + wid] == "X" }
+        if @board.grid[0..lowest_row].any?{ |row| row[col + wid] == "X" }
           col_empty = false
           break
         end
@@ -39,26 +50,21 @@ class Game
   def play
     display_turn
     while true 
-      empty_column = find_empty_column
-      if empty_column
-        @board.place_piece(@next_piece, empty_column)
-      else
-        place_piece_randomly
-      end
+      @board.place_piece(@next_piece, find_best_column)
       display_turn
       full_rows = @board.find_full_rows
       unless full_rows.empty?
-        sleep(0.2)
+        sleep(0.25)
         @board.clear_full_rows(full_rows)
         display_turn
       end
-      sleep(0.5)
+      sleep(0.75)
     end
   end
   
-  def place_piece_randomly
-    @board.place_piece(@next_piece, rand(11 - @next_piece.width))
-  end
+  # def place_piece_randomly
+  #   @board.place_piece(@next_piece, rand(11 - @next_piece.width))
+  # end
   
   def random_piece
     case rand(2)
